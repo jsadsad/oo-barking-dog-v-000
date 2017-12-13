a1f487e0cb8b9cdbd94aadbcfc4183af7b1237d7
Joshua Clarence Sadsad <joshsad2@gmail.com>
2017-12-13 22:40:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Dog
+  def initialize(name, bark = "woof!")
+    @name = name
+    @bark = bark
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def bark
+    @bark
+  end 
